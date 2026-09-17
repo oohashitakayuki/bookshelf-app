f389b026a58319cf7d88eae1352c2f045d83f240
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-09-17 16:16:41 +0900

    時間設定を日本時間に変更および言語設定を日本語に変更

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Tokyo',
 
     /*
     |--------------------------------------------------------------------------
@@ -83,7 +83,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ja',
 
     /*
     |--------------------------------------------------------------------------
@@ -167,6 +167,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
